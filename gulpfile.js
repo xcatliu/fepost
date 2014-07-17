7a98d10d2d05faa61241ed582908c73f400e5b82
xcatliu <xcatliu@gmail.com>
2014-07-17 18:13:38 +0800

    add copy task

--- gulpfile.js
+++ gulpfile.js
@@ -3,3 +3,9 @@ var gulp = require('gulp');
 gulp.task('default', function() {
     console.log('hello world');
 });
+
+gulp.task('copy', function() {
+    gulp.src('./posts/*')
+        .pipe(gulp.dest('./public/posts/'));
+});
+
